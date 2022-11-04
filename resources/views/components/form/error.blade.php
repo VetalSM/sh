298457b666facfc7b2531c810bd7ea93ffeac208
VitaliiSm <vitaliism1992@gmail.com>
2022-11-04 23:18:17 +0200

    style fix*

--- resources/views/components/form/error.blade.php
+++ resources/views/components/form/error.blade.php
@@ -2,8 +2,10 @@
 
 @error($name)
 
-@if($name = 'tel')
-    <p class="text-red-500 text-xs mt-2">{{$message = 'Невірний формат номеру телефона! Приклад: +380...'}}</p>
+@if($name === 'tel')
+    <p class="text-red-500 text-4xl lg:text-sm mt-2">{{$message = 'Невірний формат номеру телефона! Приклад: +380...'}}</p>
+    @elseif($name === 'email')
+        <p class="text-red-500 text-4xl lg:text-sm mt-2">{{$message = 'Невірний email або password'}}</p>
 @else
     <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
 @endif
