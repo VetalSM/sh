47be11b00cc5e86c6fc7e0b523e8e17ed823a4c4
VitaliiSm <vitaliism1992@gmail.com>
2022-11-06 01:21:03 +0200

    s_bag

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -1,8 +1,8 @@
 <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
       integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-<main class="max-w-6xl text-2xl lg:text-sm mx-auto my-8">
-    <div class=" px-6 mx-auto text-2xl lg:text-sm">
+<main class="max-w-6xl mx-auto my-8">
+    <div class=" px-6 mx-auto">
         <div class="flex justify-center my-6">
             <div class="flex flex-col w-full p-8 text-gray-800 bg-white shadow-lg pin-r pin-y md:w-4/5 lg:w-4/5">
                 @if ($message = Session::get('success'))
@@ -12,9 +12,9 @@
                 @endif
                 <h3 class="text-3xl text-bold">Кошик</h3>
                 <div class="flex-1">
-                    <table class="w-full md:table-fixed " cellspacing="0">
+                    <table class="w-full md:table-fixed" cellspacing="0">
                         <thead>
-                        <tr class="h-12 uppercase text-2xl lg:text-sm">
+                        <tr class="h-12 uppercase">
                             <th class="hidden md:table-cell"></th>
                             <th class="text-left">Найменування</th>
                             <th class="pl-5 text-left lg:text-center lg:pl-5">
@@ -27,7 +27,7 @@
                         </thead>
                         <tbody>
                         @foreach ($cartItems as $item)
-                            <tr class="text-2xl lg:text-sm">
+                            <tr>
                                 <td class="hidden pb-4 md:table-cell">
                                     <a href="#">
                                         <img src="{{ asset('storage/' . $item->attributes->image )}}" alt="image"
@@ -52,14 +52,14 @@
                                 </td>
                                 <td class="justify-center mt-8 md:justify-end md:flex">
                                     <div class="h-10 w-28">
-                                        <div class="relative text-2xl lg:text-sm flex flex-row w-full h-8">
+                                        <div class="relative  flex flex-row w-full h-8">
 
                                             <form action="{{ route('cart.update') }}" method="POST">
                                                 @csrf
                                                 <input type="hidden" name="id" value="{{ $item->id}}">
                                                 <input type="number" name="quantity" value="{{ $item->quantity }}"
                                                        class="w-7 text-center bg-gray-300"/>
-                                                <button type="submit" class="px-1  ml-2 text-white  rounded-xl bg-blue-500">
+                                                <button type="submit" class="px-1  ml-2 text-white rounded-xl bg-blue-500">
                                                     оновити
                                                 </button>
                                             </form>
@@ -67,15 +67,15 @@
                                     </div>
                                 </td>
                                 <td class="hidden  text-right md:table-cell "  style="padding: 0px 0px 22px 0;">
-                                <span class="ttext-2xl lg:text-sm font-medium lg:text-base  max-height">
+                                <span class="text-sm font-medium lg:text-base  max-height">
                                     {{ $item->price*$item->quantity }} грн
                                 </span>
                                 </td>
-                                <td class="text-right text-2xl lg:text-sm md:table-cell " style="padding: 0px 25px 4px 0;">
+                                <td class="text-right md:table-cell " style="padding: 0px 25px 4px 0;">
                                     <form action="{{ route('cart.remove') }}" method="POST">
                                         @csrf
                                         <input type="hidden" value="{{ $item->id }}" name="id">
-                                        <button class="px-3 text-2xl lg:text-sm py-0.5 text-white bg-red-600  rounded-full">x</button>
+                                        <button class="px-3 py-0.5 text-white bg-red-600  rounded-full">x</button>
                                     </form>
 
                                 </td>
@@ -85,7 +85,7 @@
                         </tbody>
                     </table>
 
-                    <div class="text-2xl lg:text-xl">
+                    <div class="text-xl">
                         Загальна вартість: {{ Cart::getTotal() }} грн
                     </div>
                     <br>
@@ -97,7 +97,7 @@
 
                         <form action="{{route('order')}}" method="post">
                             @csrf
-                            <div class="form-group text-2xl lg:text-sm">
+                            <div class="form-group">
                                 {{--@dd($cartItems)--}}
                                 <h3 style="color: #000000" for="escola">Контактні дані:</h3>
                                 <input type="hidden" value="{{$cartItems}}" name="name">
