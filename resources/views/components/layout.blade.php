52014b4049bd670698c66f8ada76e89de694ff3d
VitaliiSm <vitaliism1992@gmail.com>
2022-11-12 22:42:23 +0200

    *style

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,15 +1,15 @@
 <!doctype html>
 
 <html lang="ua">
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title>@yield('title', 'Madeis')</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<meta http-equiv="x-ua-compatible" content="ie=edge">
+<title>@yield('title', 'Madeis')</title>
 
-    <meta name="description" content="@yield('description')">
-    <meta name="keywords" content="@yield('keywords')">
-    <link rel="canonical" href="{{url()->current()}}"/>
-    <meta name="csrf-token" content="{{ csrf_token() }}" />
+<meta name="description" content="@yield('description')">
+<meta name="keywords" content="@yield('keywords')">
+<link rel="canonical" href="{{url()->current()}}"/>
+<meta name="csrf-token" content="{{ csrf_token() }}" />
 <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
 <link rel="preconnect" href="https://fonts.gstatic.com">
 <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap" rel="stylesheet">
@@ -44,120 +44,121 @@
 
             </a>
         </div>
-{{--        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">--}}
-{{--            Launch demo modal--}}
-{{--        </button>--}}
+        {{--        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">--}}
+        {{--            Launch demo modal--}}
+        {{--        </button>--}}
 
-{{--        <!-- Modal -->--}}
-{{--        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">--}}
-{{--            <div class="modal-dialog modal-xl">--}}
-{{--                <div class="modal-content">--}}
-{{--                    <div class="modal-header">--}}
-{{--                        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>--}}
-{{--                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>--}}
-{{--                    </div>--}}
-{{--                    <div class="modal-body">--}}
-{{--                        ...--}}
-{{--                    </div>--}}
-{{--                    <div class="modal-footer">--}}
-{{--                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>--}}
-{{--                        <button type="button" class="btn btn-primary">Save changes</button>--}}
-{{--                    </div>--}}
-{{--                </div>--}}
-{{--            </div>--}}
-{{--        </div>--}}
-
-
-{{--        <div class="hidden lg:flex justify-between mb-6">--}}
-{{--            <a href="/"--}}
-{{--               class="transition-colors duration-300 relative inline-flex items-center text-lg hover:text-blue-500">--}}
-{{--                <svg width="22" height="22" viewBox="0 0 22 22" class="mr-2">--}}
-{{--                    <g fill="none" fill-rule="evenodd">--}}
-{{--                        <path stroke="#000" stroke-opacity=".012" stroke-width=".5" d="M21 1v20.16H.84V1z">--}}
-{{--                        </path>--}}
-{{--                        <path class="fill-current"--}}
-{{--                              d="M13.854 7.224l-3.847 3.856 3.847 3.856-1.184 1.184-5.04-5.04 5.04-5.04z">--}}
-{{--                        </path>--}}
-{{--                    </g>--}}
-{{--                </svg>--}}
-
-{{--                Back to products--}}
-{{--            </a>--}}
+        {{--        <!-- Modal -->--}}
+        {{--        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">--}}
+        {{--            <div class="modal-dialog modal-xl">--}}
+        {{--                <div class="modal-content">--}}
+        {{--                    <div class="modal-header">--}}
+        {{--                        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>--}}
+        {{--                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>--}}
+        {{--                    </div>--}}
+        {{--                    <div class="modal-body">--}}
+        {{--                        ...--}}
+        {{--                    </div>--}}
+        {{--                    <div class="modal-footer">--}}
+        {{--                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>--}}
+        {{--                        <button type="button" class="btn btn-primary">Save changes</button>--}}
+        {{--                    </div>--}}
+        {{--                </div>--}}
+        {{--            </div>--}}
+        {{--        </div>--}}
 
 
-{{--                <x-category-button :category="$post->category"/>--}}
-{{--            </div>--}}
+        {{--        <div class="hidden lg:flex justify-between mb-6">--}}
+        {{--            <a href="/"--}}
+        {{--               class="transition-colors duration-300 relative inline-flex items-center text-lg hover:text-blue-500">--}}
+        {{--                <svg width="22" height="22" viewBox="0 0 22 22" class="mr-2">--}}
+        {{--                    <g fill="none" fill-rule="evenodd">--}}
+        {{--                        <path stroke="#000" stroke-opacity=".012" stroke-width=".5" d="M21 1v20.16H.84V1z">--}}
+        {{--                        </path>--}}
+        {{--                        <path class="fill-current"--}}
+        {{--                              d="M13.854 7.224l-3.847 3.856 3.847 3.856-1.184 1.184-5.04-5.04 5.04-5.04z">--}}
+        {{--                        </path>--}}
+        {{--                    </g>--}}
+        {{--                </svg>--}}
+
+        {{--                Back to products--}}
+        {{--            </a>--}}
+
+
+        {{--                <x-category-button :category="$post->category"/>--}}
+        {{--            </div>--}}
 
 
 
         <div class="mt-8 md:mt-0 flex items-center  px-15">
-            <a href="{{ route('cart.list') }}" class="flex items-center">
+            <a href="{{ route('cart.list') }}" class="flex mr-18 items-center" >
                 <img src="/images/cart.png" alt="cart" width="40" height="20">
                 <i class="ya-share1__item text-lg lg:text-xl ">  <span class="hidden  lg:inline-flex  ">  Всього:</span>  {{ Cart::getTotal() }} грн</i>
             </a>
-
-            <a href="/contact"
-               class=" text-sm lg:text-base text-black ">
-                Контакти
-            </a>
-            @auth
-
-                <x-dropdown>
-                    <x-slot name="trigger">
-                        <button class="text-xl ml-6 lg:text-sm font-bold ">
-                            Вітаю, {{ auth()->user()->name }}!
-                        </button>
-                    </x-slot>
-
-                    @admin
-
-                    <x-dropdown-item
-                        href="/admin/products"
-                        :active="request()->is('admin/products')"
-                    >
-                        Dashboard
-                    </x-dropdown-item>
-
-                    <x-dropdown-item
-                        href="/admin/products/create"
-                        :active="request()->is('admin/products/create')"
-                    >
-                        New Product
-                    </x-dropdown-item>
-                    @endadmin
-
-                    <x-dropdown-item
-                        href="#"
-                        x-data="{}"
-                        @click.prevent="document.querySelector('#logout-form').submit()"
-                    >
-                        Вихід
-                    </x-dropdown-item>
-
-                    <form id="logout-form" method="POST" action="/logout" class="hidden">
-                        @csrf
-                    </form>
-                </x-dropdown>
-            @else
-{{--                <a href="/contact"--}}
-{{--                   class="  text-black ">--}}
-{{--                    Контакти--}}
-{{--                </a>--}}
-                <a href="/register"
-                   class="ml-2 text-sm lg:text-base font-bold uppercase {{ request()->is('register') ? 'text-blue-500' : '' }}">
-                    Реєстрація
+            <div class="" style="margin-left: auto;margin-right: 0;" >
+                <a href="/contact"
+                   class=" text-sm lg:text-base  text-black ">
+                    Контакти
                 </a>
+                @auth
 
-                <a href="/login"
-                   class="ml-2 text-sm lg:text-base font-bold uppercase {{ request()->is('login') ? 'text-blue-500' : '' }}">
-                    Вхід
-                </a>
-            @endauth
+                    <x-dropdown>
+                        <x-slot name="trigger">
+                            <button class="text-xl ml-6 lg:text-sm font-bold ">
+                                Вітаю, {{ auth()->user()->name }}!
+                            </button>
+                        </x-slot>
 
-            {{--                <a href="#newsletter"--}}
-            {{--                   class="bg-blue-500 ml-3 rounded-full text-xs font-semibold text-white uppercase py-3 px-5">--}}
-            {{--                    Subscribe for Updates--}}
-            {{--                </a>--}}
+                        @admin
+
+                        <x-dropdown-item
+                            href="/admin/products"
+                            :active="request()->is('admin/products')"
+                        >
+                            Dashboard
+                        </x-dropdown-item>
+
+                        <x-dropdown-item
+                            href="/admin/products/create"
+                            :active="request()->is('admin/products/create')"
+                        >
+                            New Product
+                        </x-dropdown-item>
+                        @endadmin
+
+                        <x-dropdown-item
+                            href="#"
+                            x-data="{}"
+                            @click.prevent="document.querySelector('#logout-form').submit()"
+                        >
+                            Вихід
+                        </x-dropdown-item>
+
+                        <form id="logout-form" method="POST" action="/logout" class="hidden">
+                            @csrf
+                        </form>
+                    </x-dropdown>
+                @else
+                    {{--                <a href="/contact"--}}
+                    {{--                   class="  text-black ">--}}
+                    {{--                    Контакти--}}
+                    {{--                </a>--}}
+                    <a href="/register"
+                       class="ml-2 text-sm lg:text-base font-bold uppercase {{ request()->is('register') ? 'text-blue-500' : '' }}">
+                        Реєстрація
+                    </a>
+
+                    <a href="/login"
+                       class="ml-2 text-sm lg:text-base font-bold uppercase {{ request()->is('login') ? 'text-blue-500' : '' }}">
+                        Вхід
+                    </a>
+                @endauth
+
+                {{--                <a href="#newsletter"--}}
+                {{--                   class="bg-blue-500 ml-3 rounded-full text-xs font-semibold text-white uppercase py-3 px-5">--}}
+                {{--                    Subscribe for Updates--}}
+                {{--                </a>--}}
+            </div>
         </div>
     </nav>
 
