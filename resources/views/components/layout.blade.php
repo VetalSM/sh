2abebd44b3636ea71dec549adff461beb6611e35
VitaliiSm <vitaliism1992@gmail.com>
2022-12-15 21:28:39 +0200

    «*»

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,6 +1,13 @@
 <!doctype html>
-
 <html lang="ua">
+<head>
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-KX8DH3S');</script>
+    <!-- End Google Tag Manager -->
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 <meta http-equiv="x-ua-compatible" content="ie=edge">
@@ -38,6 +45,10 @@
 </style>
 
 <body style="font-family: Open Sans, sans-serif">
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KX8DH3S"
+                  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
 <div class="  bg-gray-100 border-opacity-5  text-right py-1 px-4 ">
     <div class="py-1">
         <div class="px-3  inline">
