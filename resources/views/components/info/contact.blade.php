04cd56beb8afeedd14759981afb22c4cefa45bbe
VitaliiSm <vitaliism1992@gmail.com>
2022-12-15 22:15:20 +0200

    «*»

--- resources/views/components/info/contact.blade.php
+++ resources/views/components/info/contact.blade.php
@@ -1,19 +1,45 @@
 
-    <x-layout>  contact
-        <div>
-            <a href="/"
-               class="transition-colors duration-300 relative inline-flex  hover:text-blue-500">
-                <svg width="22" height="22" viewBox="0 0 22 22" class="mr-2">
-                    <g fill="none" fill-rule="evenodd">
-                        <path stroke="#000" stroke-opacity=".012" stroke-width=".5" d="M21 1v20.16H.84V1z">
-                        </path>
-                        <path class="fill-current"
-                              d="M13.854 7.224l-3.847 3.856 3.847 3.856-1.184 1.184-5.04-5.04 5.04-5.04z">
-                        </path>
-                    </g>
-                </svg>
-                До каталогу
-            </a>
+    <x-layout>
+        <div class="card text-left  mx-auto" style="width: 50rem;">
+            <div class="card-body">
+                <h3 class="card-title text-center " style="padding-bottom: 15px;"><span class="text-secondary">Контакти</span></h3>
+                <p class="card-text">
+                    Ви можете зв'язатись з нами за номером телефону: <br/>
+                    <span class="text-danger"> +380955600435 <br/>
+                    e-mail: support@madeis.com.ua</span><p/>
 
+                    Або приєднуйтесь до наших спільнот у меседжерах: <br/>
+                <a href="https://www.facebook.com/madeis.ua"
+                >Facebook
+                </a><br/>
+                <a href="https://www.instagram.com/madeis.ua/"
+                >Instagram
+                </a><br/>
+                <a href="https://t.me/MadeIS_UA"
+                >Telegram
+                </a> <p/>
+                    Графік роботи:<br/>
+                    ПН-ПТ: з 9.00 до 18.00 <br/>
+                <span class="text-danger"> СБ: вихідний день</span> <br/>
+                    НД: з 10.00 до 16.00 <p/>
+
+
+
+
+                </p>
+                <a href="/"
+                   class="transition-colors duration-300 relative inline-flex  hover:text-blue-500">
+                    <svg width="22" height="22" viewBox="0 0 22 22" class="mr-2">
+                        <g fill="none" fill-rule="evenodd">
+                            <path stroke="#000" stroke-opacity=".012" stroke-width=".5" d="M21 1v20.16H.84V1z">
+                            </path>
+                            <path class="fill-current"
+                                  d="M13.854 7.224l-3.847 3.856 3.847 3.856-1.184 1.184-5.04-5.04 5.04-5.04z">
+                            </path>
+                        </g>
+                    </svg>
+                    До каталогу
+                </a>
+            </div>
         </div></x-layout>
 
