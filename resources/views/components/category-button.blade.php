cae6c0dc36ead58cf5e37d33fd6cfff475610699
VitaliiSm <vitaliism1992@gmail.com>
2022-12-21 22:05:50 +0200

    s_bag

--- resources/views/components/category-button.blade.php
+++ resources/views/components/category-button.blade.php
@@ -1,6 +1,6 @@
 @props(['category'])
 
 <a href="/?category={{ $category->slug }}"
-   class="px-3 py-1 border border-red-800 rounded-full text-xs text-blue-450 uppercase font-semibold"
+   class="px-3 py-1 border border-red-800 rounded-full text-xs text-blue-450 font-semibold"
 
 >{{ $category->name }}</a>
