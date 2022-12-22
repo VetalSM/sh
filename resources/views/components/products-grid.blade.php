42426cff563fd9fa377500cc253e143a67fd5bf6
VitaliiSm <vitaliism1992@gmail.com>
2022-12-22 12:36:36 +0200

    *

--- resources/views/components/products-grid.blade.php
+++ resources/views/components/products-grid.blade.php
@@ -5,9 +5,13 @@
 @if ($products->count() >= 0)
     <div class="lg:grid lg:grid-cols-6">
         @foreach ($products->skip(0) as $product)
+            @if($product->status !== "not_available")
             <x-product-card
                 :product="$product"
                 class="{{ $loop->iteration < 3 ? 'col-span-2' : 'col-span-2' }}"></x-product-card>
+            @else
+
+            @endif
         @endforeach
     </div>
 @endif
