1b394b7f6c914a06cb968d3181bbf69de22ba796
VitaliiSm <vitaliism1992@gmail.com>
2022-12-25 19:40:15 +0200

    *

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -36,6 +36,7 @@
     <link rel="apple-touch-icon" sizes="180x180" href="/public/images/title/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="/public/images/title/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="/public/images/title/favicon-16x16.png">
+    <link rel ="icon" type = "image/x-icon" href ="/public/images/title/title.png">
     <link rel="manifest" href="/public/images/title/site.webmanifest">
     <link rel="mask-icon" href="/public/images/title/safari-pinned-tab.svg" color="#5bbad5">
     <meta name="msapplication-TileColor" content="#da532c">
