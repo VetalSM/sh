aecf50afa118db94a2c3fdf07c087fd86a4b57f7
VitaliiSm <vitaliism1992@gmail.com>
2022-12-25 21:12:46 +0200

    *

--- resources/views/components/category-dropdown.blade.php
+++ resources/views/components/category-dropdown.blade.php
@@ -1,7 +1,7 @@
 <x-dropdown>
     <x-slot name="trigger">
-        <button class="py-2 pl-3 pr-9 font-semibold w-full lg:w-32 text-left flex lg:inline-flex">
-            {{ isset($currentCategory) ? ucwords($currentCategory->name) : 'Категорії' }}
+        <button class="py-2 pl-3 pr-9 font-semibold w-full lg:w-50 text-left flex lg:inline-flex">
+            {{ isset($currentCategory) ? ucwords($currentCategory->name) : 'Всі категорії' }}
 
             <x-icon name="down-arrow" class="absolute pointer-events-none" style="right: 12px;"/>
         </button>
