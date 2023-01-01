cb31bc3504edd69b2eeb2e5e21a46a3103a1edf5
VitaliiSm <vitaliism1992@gmail.com>
2023-01-01 18:37:32 +0200

    *

--- resources/views/components/status-product.blade.php
+++ resources/views/components/status-product.blade.php
@@ -3,16 +3,16 @@
 @if($product->status=== 'new')
    <div class=" text-center bd_item bd_item_new">
 
-        <img src="{{ asset('storage/' . $product->thumbnail) }}"
-             alt="" class=" rounded-xl" style="display: block;
+        <img loading="lazy" src="{{ asset('storage/' . $product->thumbnail) }}"
+             alt="{{$product->title}}" class=" rounded-xl" style="display: block;
                                                         margin-left: auto;
                                                         margin-right: auto;
                                                         width:100%;">
         </div>
 @elseif($product->status==="promotion")
                 <div class=" text-center bd_item bd_item_Promotion">
-                    <img src="{{ asset('storage/' . $product->thumbnail) }}"
-                         alt="" class=" rounded-xl" style="display: block;
+                    <img loading="lazy" src="{{ asset('storage/' . $product->thumbnail) }}"
+                         alt="{{$product->title}}" class=" rounded-xl" style="display: block;
                                                         margin-left: auto;
                                                         margin-right: auto;
                                                         width:100%;">
@@ -20,8 +20,8 @@
 @elseif($product->status==="sale")
     <div class=" text-center ">
 
-        <img src="{{ asset('storage/' . $product->thumbnail) }}"
-             alt="" class=" rounded-xl" style="display: block;
+        <img  loading="lazy" src="{{ asset('storage/' . $product->thumbnail) }}"
+             alt="{{$product->title}}" class=" rounded-xl" style="display: block;
                                                         margin-left: auto;
                                                         margin-right: auto;
                                                         width:100%;">
@@ -30,8 +30,8 @@
 @elseif($product->status==="hit")
     <div class=" text-center ">
 
-        <img src="{{ asset('storage/' . $product->thumbnail) }}"
-             alt="" class=" rounded-xl" style="display: block;
+        <img  loading="lazy" src="{{ asset('storage/' . $product->thumbnail) }}"
+             alt="{{$product->title}}" class=" rounded-xl" style="display: block;
                                                         margin-left: auto;
                                                         margin-right: auto;
                                                         width:100%;">
@@ -40,8 +40,8 @@
 @elseif($product->status==="expect")
     <div class=" text-center ">
 
-        <img src="{{ asset('storage/' . $product->thumbnail) }}"
-             alt="" class=" rounded-xl" style="display: block;
+        <img loading="lazy" src="{{ asset('storage/' . $product->thumbnail) }}"
+             alt="{{$product->title}}" class=" rounded-xl" style="display: block;
                                                         margin-left: auto;
                                                         margin-right: auto;
                                                         width:100%;">
@@ -50,8 +50,8 @@
 @else
                             <div class=" text-center ">
 
-                                <img src="{{ asset('storage/' . $product->thumbnail) }}"
-                                     alt="" class=" rounded-xl" style="display: block;
+                                <img loading="lazy" src="{{ asset('storage/' . $product->thumbnail) }}"
+                                     alt="{{$product->title}}" class=" rounded-xl" style="display: block;
                                                         margin-left: auto;
                                                         margin-right: auto;
                                                         width:100%;">
