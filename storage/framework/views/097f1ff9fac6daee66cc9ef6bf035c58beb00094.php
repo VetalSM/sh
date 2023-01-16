9e1c8ccc9a83528d56a4c3415cc6a0aa20b8c993
VitaliiSm <vitaliism1992@gmail.com>
2023-01-16 21:44:39 +0200

    *

--- storage/framework/views/097f1ff9fac6daee66cc9ef6bf035c58beb00094.php
+++
@@ -1,5 +0,0 @@
-<div class="mt-6">
-    <?php echo e($slot); ?>
-
-</div>
-<?php /**PATH /home/vitalii/Desktop/PhpstormProjects/sp/test price/shop/resources/views/components/form/field.blade.php ENDPATH**/ ?>
--- storage/framework/views/1a98a9c22fad54d79f077b0c8f6a97f727950b92.php
+++
@@ -1,5 +0,0 @@
-<div <?php echo e($attributes(['class' => 'border border-gray-200 p-6 rounded-xl'])); ?>>
-    <?php echo e($slot); ?>
-
-</div>
-<?php /**PATH /home/vitalii/Desktop/PhpstormProjects/sp/test price/shop/resources/views/components/panel.blade.php ENDPATH**/ ?>
