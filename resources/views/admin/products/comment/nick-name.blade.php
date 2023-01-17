9eeeb8d148dd2009cf2577f96907952914e52cab
VitaliiSm <vitaliism1992@gmail.com>
2023-01-17 17:50:15 +0200

    *

--- resources/views/admin/products/comment/nick-name.blade.php
+++ resources/views/admin/products/comment/nick-name.blade.php
@@ -1,12 +1,36 @@
 <x-layout>
     <x-setting heading="Manage Comments">
-                            @foreach (\App\Models\Comment::all() as $comment)
-            {{ $comment->nickName }}<br/>
-                            @endforeach
-        --------------------------------------------------<br/>
-                                @foreach (\App\Models\Comment::all() as $comment)
-                                    {{ $comment->name }}<br/>
-                                @endforeach
+
+
+
+                                <div class="flex">
+
+                                    <main class="flex-1">
+                                        <x-panel>
+                                            <h4>Тел</h4>
+                                            @foreach (\App\Models\Comment::all() as $comment)
+                                                {{ $comment->tel }}<br/>
+                                            @endforeach
+                                        </x-panel>
+                                    </main>
+                                    <main class="flex-2">
+                                        <x-panel>
+                                            <h4>Никнейм</h4>
+                                            @foreach (\App\Models\Comment::all() as $comment)
+                                                {{ $comment->nickName }}<br/>
+                                            @endforeach
+                                        </x-panel>
+                                    </main>
+                                    <main class="flex-3">
+                                        <x-panel>
+                                            <h4>Имя</h4>
+                                            @foreach (\App\Models\Comment::all() as $comment)
+                                                {{ $comment->name }}<br/>
+                                            @endforeach
+                                        </x-panel>
+                                    </main>
+                                </div>
+
 
     </x-setting>
 </x-layout>
