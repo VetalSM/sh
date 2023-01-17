897bbfa64d2626b99ff700e2e3f8d3c28e4b7d55
VitaliiSm <vitaliism1992@gmail.com>
2023-01-17 15:25:14 +0200

    *

--- resources/views/admin/products/comment/nick-name.blade.php
+++ resources/views/admin/products/comment/nick-name.blade.php
@@ -3,6 +3,10 @@
                             @foreach (\App\Models\Comment::all() as $comment)
             {{ $comment->nickName }}<br/>
                             @endforeach
+        --------------------------------------------------<br/>
+                                @foreach (\App\Models\Comment::all() as $comment)
+                                    {{ $comment->name }}<br/>
+                                @endforeach
 
     </x-setting>
 </x-layout>
