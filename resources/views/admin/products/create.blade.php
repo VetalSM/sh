4fbf69bfcf99cb7a0fff4ec927e12d1df59cef49
VitaliiSm <vitaliism1992@gmail.com>
2023-04-09 18:50:01 +0300

    *

--- resources/views/admin/products/create.blade.php
+++ resources/views/admin/products/create.blade.php
@@ -11,28 +11,66 @@
             <x-form.input name="safety" type="file" />
             <x-form.textarea name="excerpt" required />
 {{--            <x-form.textarea name="body" required />--}}
-            <textarea id="content" name="body">h</textarea>
+            <textarea id="body" name="body">h</textarea>
 
-            <script src="https://cdn.tiny.cloud/1/pebtcux3vb4jvpk5xu5eqdmmxiohb4tj9plx25aken3kenzs/tinymce/6/tinymce.min.js" referrerpolicy="origin"></script>
             <script>
                 tinymce.init({
-                    selector: '#content',
-                    plugins: 'anchor autolink charmap code codesample emoticons image link lists media searchreplace table visualblocks wordcount ',
-                    toolbar: 'undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image | code',
-                    relative_urls: false,
-                    file_picker_callback: function(callback, value, meta) {
-                        if (meta.filetype == 'image') {
-                            tinymce.activeEditor.windowManager.openUrl({
-                                url: '/laravel-filemanager?type=' + meta.filetype,
-                                title: 'File Manager',
-                                width: 900,
-                                height: 600,
-                                onMessage: function(api, message) {
-                                    callback(message.content);
-                                }
-                            });
+                    selector: '#body',
+                    language: 'ru',
+                    plugins: 'preview importcss searchreplace autolink autosave save directionality code visualblocks visualchars fullscreen image link media template codesample table charmap pagebreak nonbreaking anchor insertdatetime advlist lists wordcount help charmap quickbars emoticons',
+                    imagetools_cors_hosts: ['picsum.photos'],
+                    menubar: 'file edit view insert format tools table help',
+                    toolbar: 'undo redo | bold italic underline strikethrough | fontfamily fontsize blocks | alignleft aligncenter alignright alignjustify | outdent indent |  numlist bullist | forecolor backcolor removeformat | pagebreak | charmap emoticons | fullscreen  preview save print | insertfile image media template link anchor codesample | ltr rtl',
+                    toolbar_sticky: true,
+                    autosave_ask_before_unload: true,
+                    autosave_interval: "30s",
+                    autosave_prefix: "{path}{query}-{id}-",
+                    autosave_restore_when_empty: false,
+                    autosave_retention: "2m",
+                    image_advtab: true,
+                    content_css: '//www.tiny.cloud/css/codepen.min.css',
+                    link_list: [
+                        { title: 'My page 1', value: 'http://www.tinymce.com' },
+                        { title: 'My page 2', value: 'http://www.moxiecode.com' }
+                    ],
+                    image_list: [
+                        { title: 'My page 1', value: 'http://www.tinymce.com' },
+                        { title: 'My page 2', value: 'http://www.moxiecode.com' }
+                    ],
+                    image_class_list: [
+                        { title: 'None', value: '' },
+                        { title: 'Some class', value: 'class-name' }
+                    ],
+                    importcss_append: true,
+                    file_picker_callback: function (callback, value, meta) {
+                        /* Provide file and text for the link dialog */
+                        if (meta.filetype === 'file') {
+                            callback('https://www.google.com/logos/google.jpg', { text: 'My text' });
                         }
-                    }
+
+                        /* Provide image and alt text for the image dialog */
+                        if (meta.filetype === 'image') {
+                            callback('https://www.google.com/logos/google.jpg', { alt: 'My alt text' });
+                        }
+
+                        /* Provide alternative source and posted for the media dialog */
+                        if (meta.filetype === 'media') {
+                            callback('movie.mp4', { source2: 'alt.ogg', poster: 'https://www.google.com/logos/google.jpg' });
+                        }
+                    },
+                    templates: [
+                        { title: 'New Table', description: 'creates a new table', content: '<div class="mceTmpl"><table width="98%%"  border="0" cellspacing="0" cellpadding="0"><tr><th scope="col"> </th><th scope="col"> </th></tr><tr><td> </td><td> </td></tr></table></div>' },
+                        { title: 'Starting my story', description: 'A cure for writers block', content: 'Once upon a time...' },
+                        { title: 'New list with dates', description: 'New List with dates', content: '<div class="mceTmpl"><span class="cdate">cdate</span><br /><span class="mdate">mdate</span><h2>My List</h2><ul><li></li><li></li></ul></div>' }
+                    ],
+                    template_cdate_format: '[Date Created (CDATE): %m/%d/%Y : %H:%M:%S]',
+                    template_mdate_format: '[Date Modified (MDATE): %m/%d/%Y : %H:%M:%S]',
+                    height: 520,
+                    image_caption: true,
+                    quickbars_selection_toolbar: 'bold italic | quicklink h2 h3 blockquote quickimage quicktable',
+                    noneditable_noneditable_class: "mceNonEditable",
+                    toolbar_mode: 'sliding',
+                    contextmenu: "link image imagetools table",
                 });
             </script>
             <x-form.textarea name="meta_title" ></x-form.textarea>
@@ -56,7 +94,67 @@
             </x-form.field>
             <x-form.input name="title_ru" />
             <x-form.textarea name="excerpt_ru" />
-            <x-form.textarea name="body_ru" />
+            <x-form.textarea id="body_ru" name="body_ru" />
+            <script>
+                tinymce.init({
+                    selector: '#body_ru',
+                    language: 'ru',
+                    plugins: 'preview importcss searchreplace autolink autosave save directionality code visualblocks visualchars fullscreen image link media template codesample table charmap pagebreak nonbreaking anchor insertdatetime advlist lists wordcount help charmap quickbars emoticons',
+                    imagetools_cors_hosts: ['picsum.photos'],
+                    menubar: 'file edit view insert format tools table help',
+                    toolbar: 'undo redo | bold italic underline strikethrough | fontfamily fontsize blocks | alignleft aligncenter alignright alignjustify | outdent indent |  numlist bullist | forecolor backcolor removeformat | pagebreak | charmap emoticons | fullscreen  preview save print | insertfile image media template link anchor codesample | ltr rtl',
+                    toolbar_sticky: true,
+                    autosave_ask_before_unload: true,
+                    autosave_interval: "30s",
+                    autosave_prefix: "{path}{query}-{id}-",
+                    autosave_restore_when_empty: false,
+                    autosave_retention: "2m",
+                    image_advtab: true,
+                    content_css: '//www.tiny.cloud/css/codepen.min.css',
+                    link_list: [
+                        { title: 'My page 1', value: 'http://www.tinymce.com' },
+                        { title: 'My page 2', value: 'http://www.moxiecode.com' }
+                    ],
+                    image_list: [
+                        { title: 'My page 1', value: 'http://www.tinymce.com' },
+                        { title: 'My page 2', value: 'http://www.moxiecode.com' }
+                    ],
+                    image_class_list: [
+                        { title: 'None', value: '' },
+                        { title: 'Some class', value: 'class-name' }
+                    ],
+                    importcss_append: true,
+                    file_picker_callback: function (callback, value, meta) {
+                        /* Provide file and text for the link dialog */
+                        if (meta.filetype === 'file') {
+                            callback('https://www.google.com/logos/google.jpg', { text: 'My text' });
+                        }
+
+                        /* Provide image and alt text for the image dialog */
+                        if (meta.filetype === 'image') {
+                            callback('https://www.google.com/logos/google.jpg', { alt: 'My alt text' });
+                        }
+
+                        /* Provide alternative source and posted for the media dialog */
+                        if (meta.filetype === 'media') {
+                            callback('movie.mp4', { source2: 'alt.ogg', poster: 'https://www.google.com/logos/google.jpg' });
+                        }
+                    },
+                    templates: [
+                        { title: 'New Table', description: 'creates a new table', content: '<div class="mceTmpl"><table width="98%%"  border="0" cellspacing="0" cellpadding="0"><tr><th scope="col"> </th><th scope="col"> </th></tr><tr><td> </td><td> </td></tr></table></div>' },
+                        { title: 'Starting my story', description: 'A cure for writers block', content: 'Once upon a time...' },
+                        { title: 'New list with dates', description: 'New List with dates', content: '<div class="mceTmpl"><span class="cdate">cdate</span><br /><span class="mdate">mdate</span><h2>My List</h2><ul><li></li><li></li></ul></div>' }
+                    ],
+                    template_cdate_format: '[Date Created (CDATE): %m/%d/%Y : %H:%M:%S]',
+                    template_mdate_format: '[Date Modified (MDATE): %m/%d/%Y : %H:%M:%S]',
+                    height: 520,
+                    image_caption: true,
+                    quickbars_selection_toolbar: 'bold italic | quicklink h2 h3 blockquote quickimage quicktable',
+                    noneditable_noneditable_class: "mceNonEditable",
+                    toolbar_mode: 'sliding',
+                    contextmenu: "link image imagetools table",
+                });
+            </script>
             <x-form.textarea name="meta_title_ru" ></x-form.textarea>
             <x-form.textarea name="meta_description_ru"></x-form.textarea>
             <x-form.button>Publish</x-form.button>
--- resources/views/admin/products/edit.blade.php
+++ resources/views/admin/products/edit.blade.php
@@ -72,23 +72,67 @@
             <span> {{$product->safety}}</span>
 
             <x-form.textarea name="excerpt" required>{{ old('excerpt', $product->excerpt) }}</x-form.textarea>
-            <textarea id="content" name="body" required>{{ old('body', $product->body) }}</textarea>
+            <textarea id="body" name="body" required>{{ old('body', $product->body) }}</textarea>
 
             <script src="https://cdn.tiny.cloud/1/pebtcux3vb4jvpk5xu5eqdmmxiohb4tj9plx25aken3kenzs/tinymce/6/tinymce.min.js" referrerpolicy="origin"></script>
             <script>
                 tinymce.init({
-                    selector: '#content',
+                    selector: '#body',
                     language: 'ru',
-                    plugins: 'anchor autolink charmap codesample code emoticons image link lists media searchreplace table visualblocks wordcount ',
+                    plugins: 'preview importcss searchreplace autolink autosave save directionality code visualblocks visualchars fullscreen image link media template codesample table charmap pagebreak nonbreaking anchor insertdatetime advlist lists wordcount help charmap quickbars emoticons',
+                    imagetools_cors_hosts: ['picsum.photos'],
+                    menubar: 'file edit view insert format tools table help',
+                    toolbar: 'undo redo | bold italic underline strikethrough | fontfamily fontsize blocks | alignleft aligncenter alignright alignjustify | outdent indent |  numlist bullist | forecolor backcolor removeformat | pagebreak | charmap emoticons | fullscreen  preview save print | insertfile image media template link anchor codesample | ltr rtl',
+                    toolbar_sticky: true,
+                    autosave_ask_before_unload: true,
+                    autosave_interval: "30s",
+                    autosave_prefix: "{path}{query}-{id}-",
+                    autosave_restore_when_empty: false,
+                    autosave_retention: "2m",
+                    image_advtab: true,
+                    content_css: '//www.tiny.cloud/css/codepen.min.css',
+                    link_list: [
+                        { title: 'My page 1', value: 'http://www.tinymce.com' },
+                        { title: 'My page 2', value: 'http://www.moxiecode.com' }
+                    ],
+                    image_list: [
+                        { title: 'My page 1', value: 'http://www.tinymce.com' },
+                        { title: 'My page 2', value: 'http://www.moxiecode.com' }
+                    ],
+                    image_class_list: [
+                        { title: 'None', value: '' },
+                        { title: 'Some class', value: 'class-name' }
+                    ],
+                    importcss_append: true,
+                    file_picker_callback: function (callback, value, meta) {
+                        /* Provide file and text for the link dialog */
+                        if (meta.filetype === 'file') {
+                            callback('https://www.google.com/logos/google.jpg', { text: 'My text' });
+                        }
 
-                    toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck typography | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat',
-                    tinycomments_mode: 'embedded',
+                        /* Provide image and alt text for the image dialog */
+                        if (meta.filetype === 'image') {
+                            callback('https://www.google.com/logos/google.jpg', { alt: 'My alt text' });
+                        }
 
-                    tinycomments_author: 'Author name',
-                    mergetags_list: [
-                        { value: 'First.Name', title: 'First Name' },
-                        { value: 'Email', title: 'Email' },
-                    ]
+                        /* Provide alternative source and posted for the media dialog */
+                        if (meta.filetype === 'media') {
+                            callback('movie.mp4', { source2: 'alt.ogg', poster: 'https://www.google.com/logos/google.jpg' });
+                        }
+                    },
+                    templates: [
+                        { title: 'New Table', description: 'creates a new table', content: '<div class="mceTmpl"><table width="98%%"  border="0" cellspacing="0" cellpadding="0"><tr><th scope="col"> </th><th scope="col"> </th></tr><tr><td> </td><td> </td></tr></table></div>' },
+                        { title: 'Starting my story', description: 'A cure for writers block', content: 'Once upon a time...' },
+                        { title: 'New list with dates', description: 'New List with dates', content: '<div class="mceTmpl"><span class="cdate">cdate</span><br /><span class="mdate">mdate</span><h2>My List</h2><ul><li></li><li></li></ul></div>' }
+                    ],
+                    template_cdate_format: '[Date Created (CDATE): %m/%d/%Y : %H:%M:%S]',
+                    template_mdate_format: '[Date Modified (MDATE): %m/%d/%Y : %H:%M:%S]',
+                    height: 520,
+                    image_caption: true,
+                    quickbars_selection_toolbar: 'bold italic | quicklink h2 h3 blockquote quickimage quicktable',
+                    noneditable_noneditable_class: "mceNonEditable",
+                    toolbar_mode: 'sliding',
+                    contextmenu: "link image imagetools table",
                 });
             </script>
             <x-form.textarea name="meta_title" >{{ old('meta_title', $product->meta_title) }}</x-form.textarea>
@@ -97,7 +141,67 @@
 
             <x-form.input name="title_ru" :value="old('title', $product->title_ru)" required />
             <x-form.textarea name="excerpt_ru" required>{{ old('excerpt', $product->excerpt_ru) }}</x-form.textarea>
-            <x-form.textarea name="body_ru" required>{{ old('body', $product->body_ru) }}</x-form.textarea>
+            <x-form.textarea id="body_ru"  name="body_ru" required>{{ old('body', $product->body_ru) }}</x-form.textarea>
+            <script>
+                tinymce.init({
+                    selector: '#body_ru',
+                    language: 'ru',
+                    plugins: 'preview importcss searchreplace autolink autosave save directionality code visualblocks visualchars fullscreen image link media template codesample table charmap pagebreak nonbreaking anchor insertdatetime advlist lists wordcount help charmap quickbars emoticons',
+                    imagetools_cors_hosts: ['picsum.photos'],
+                    menubar: 'file edit view insert format tools table help',
+                    toolbar: 'undo redo | bold italic underline strikethrough | fontfamily fontsize blocks | alignleft aligncenter alignright alignjustify | outdent indent |  numlist bullist | forecolor backcolor removeformat | pagebreak | charmap emoticons | fullscreen  preview save print | insertfile image media template link anchor codesample | ltr rtl',
+                    toolbar_sticky: true,
+                    autosave_ask_before_unload: true,
+                    autosave_interval: "30s",
+                    autosave_prefix: "{path}{query}-{id}-",
+                    autosave_restore_when_empty: false,
+                    autosave_retention: "2m",
+                    image_advtab: true,
+                    content_css: '//www.tiny.cloud/css/codepen.min.css',
+                    link_list: [
+                        { title: 'My page 1', value: 'http://www.tinymce.com' },
+                        { title: 'My page 2', value: 'http://www.moxiecode.com' }
+                    ],
+                    image_list: [
+                        { title: 'My page 1', value: 'http://www.tinymce.com' },
+                        { title: 'My page 2', value: 'http://www.moxiecode.com' }
+                    ],
+                    image_class_list: [
+                        { title: 'None', value: '' },
+                        { title: 'Some class', value: 'class-name' }
+                    ],
+                    importcss_append: true,
+                    file_picker_callback: function (callback, value, meta) {
+                        /* Provide file and text for the link dialog */
+                        if (meta.filetype === 'file') {
+                            callback('https://www.google.com/logos/google.jpg', { text: 'My text' });
+                        }
+
+                        /* Provide image and alt text for the image dialog */
+                        if (meta.filetype === 'image') {
+                            callback('https://www.google.com/logos/google.jpg', { alt: 'My alt text' });
+                        }
+
+                        /* Provide alternative source and posted for the media dialog */
+                        if (meta.filetype === 'media') {
+                            callback('movie.mp4', { source2: 'alt.ogg', poster: 'https://www.google.com/logos/google.jpg' });
+                        }
+                    },
+                    templates: [
+                        { title: 'New Table', description: 'creates a new table', content: '<div class="mceTmpl"><table width="98%%"  border="0" cellspacing="0" cellpadding="0"><tr><th scope="col"> </th><th scope="col"> </th></tr><tr><td> </td><td> </td></tr></table></div>' },
+                        { title: 'Starting my story', description: 'A cure for writers block', content: 'Once upon a time...' },
+                        { title: 'New list with dates', description: 'New List with dates', content: '<div class="mceTmpl"><span class="cdate">cdate</span><br /><span class="mdate">mdate</span><h2>My List</h2><ul><li></li><li></li></ul></div>' }
+                    ],
+                    template_cdate_format: '[Date Created (CDATE): %m/%d/%Y : %H:%M:%S]',
+                    template_mdate_format: '[Date Modified (MDATE): %m/%d/%Y : %H:%M:%S]',
+                    height: 520,
+                    image_caption: true,
+                    quickbars_selection_toolbar: 'bold italic | quicklink h2 h3 blockquote quickimage quicktable',
+                    noneditable_noneditable_class: "mceNonEditable",
+                    toolbar_mode: 'sliding',
+                    contextmenu: "link image imagetools table",
+                });
+            </script>
             <x-form.textarea name="meta_title_ru" >{{ old('meta_title', $product->meta_title_ru) }}</x-form.textarea>
             <x-form.textarea name="meta_description_ru" >{{ old('meta_description', $product->meta_description_ru) }}</x-form.textarea>
             <x-form.button>Update</x-form.button>
