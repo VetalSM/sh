4843f7bf783470a09411ada248009369515f1e1c
VitaliiSm <vitaliism1992@gmail.com>
2023-11-22 18:31:42 +0100

    fix

--- app/Console/Commands/UpdateProductStatus.php
+++ app/Console/Commands/UpdateProductStatus.php
@@ -38,7 +38,7 @@ class UpdateProductStatus extends Command
     {
 
         foreach (Product::all() as $product) {
-            foreach (StatusJob::where('product_id', $product->id)->get() as $status)
+            foreach (StatusJob::where('product_id', $product->id)->get() as $status) {
                 if ($status->price_status == 'active') {
                     if ($product->id === $status->product_id) {
                         if ($status->status_start != 0 && $status->work_start != 1 && $status->start_date < now()) {
@@ -57,21 +57,22 @@ class UpdateProductStatus extends Command
                         }
                     }
                 }
-            if ($status->price_status == 'prom') {
-                if ($product->id === $status->product_id) {
-                    if ($status->status_start != 0 && $status->work_start != 1 && $status->start_date < now()) {
-                        $product->status = $status->status_start;
-                        $product->prom_prices = $status->price_start_name;
-                        $status->work_start = "1";
-                        $status->save();
-                        $product->save();
-                    }
-                    if ($status->status_end != 0 && $status->work_end != 1 && $status->end_date < now()) {
-                        $product->status = $status->status_end;
-                        $product->prom_prices = $status->price_end_name;
-                        $status->work_end = "1";
-                        $status->save();
-                        $product->save();
+                if ($status->price_status == 'prom') {
+                    if ($product->id === $status->product_id) {
+                        if ($status->status_start != 0 && $status->work_start != 1 && $status->start_date < now()) {
+                            $product->status = $status->status_start;
+                            $product->prom_prices = $status->price_start_name;
+                            $status->work_start = "1";
+                            $status->save();
+                            $product->save();
+                        }
+                        if ($status->status_end != 0 && $status->work_end != 1 && $status->end_date < now()) {
+                            $product->status = $status->status_end;
+                            $product->prom_prices = $status->price_end_name;
+                            $status->work_end = "1";
+                            $status->save();
+                            $product->save();
+                        }
                     }
                 }
             }
